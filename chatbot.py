179ceef8ac621f3219c860a23c33be921816b60c
joerkig <44418429+joerkig@users.noreply.github.com>
2021-04-09 19:09:13 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -57,55 +57,58 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         return
 
     def do_command(self, e, cmd, arg1):
-        c = self.connection
-        howto = "To request a map find the key of it on http://ohshapes.com and put it behind this command"   
-        notfound = "A map with that key was not found, recheck key on http://ohshapes.com"
-        
-        # Poll the API to get current game.
-        if cmd == "game":
-            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
-            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
-            r = requests.get(url, headers=headers).json()
-            c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
+        try:
+            c = self.connection
+            howto = "To request a map find the key of it on http://ohshapes.com and put it behind this command"   
+            notfound = "A map with that key was not found, recheck key on http://ohshapes.com"
+            
+            # Poll the API to get current game.
+            if cmd == "game":
+                url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+                headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+                r = requests.get(url, headers=headers).json()
+                c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
 
-        # Poll the API the get the current status of the stream
-        elif cmd == "title":
-            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
-            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
-            r = requests.get(url, headers=headers).json()
-            c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
+            # Poll the API the get the current status of the stream
+            elif cmd == "title":
+                url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+                headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+                r = requests.get(url, headers=headers).json()
+                c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
 
-        # Provide basic information to viewers for specific commands
-        elif cmd == "oslatest":
-            url = 'http://ohshapes.com/api/maps/latest/0?'
-            r = requests.get(url).json()
-            c.privmsg(self.channel, 'Most recently uploaded was ' + r['docs'][0]['metadata']['songName'] + ' by ' + r['docs'][0]['metadata']['songAuthorName'] + ' uploaded by ' + r['docs'][0]['uploader']['username'] )
-        elif cmd == "osr":  
-            if arg1 == None:   
-                c.privmsg(self.channel, howto)
+            # Provide basic information to viewers for specific commands
+            elif cmd == "oslatest":
+                url = 'http://ohshapes.com/api/maps/latest/0?'
+                r = requests.get(url).json()
+                c.privmsg(self.channel, 'Most recently uploaded was ' + r['docs'][0]['metadata']['songName'] + ' by ' + r['docs'][0]['metadata']['songAuthorName'] + ' uploaded by ' + r['docs'][0]['uploader']['username'] )
+            elif cmd == "osr":  
+                if arg1 == None:   
+                    c.privmsg(self.channel, howto)
+                else:
+                    url = 'http://ohshapes.com/api/maps/detail/'
+                    try:
+                        r = requests.get(url + arg1).json()
+                        if len(str(r['stats']['rating'])[2:4]) == 1:
+                            rating = str(r['stats']['rating'])[2:4] + "0"
+                        else:
+                            rating = str(r['stats']['rating'])[2:4]
+                        c.privmsg(self.channel, r['metadata']['songName'] + ' ' + str(rating) + '% (' + r['key'] + ') was added to requests.')
+                        url2 = 'http://ohshapes.com'
+                        r2 = requests.get(url2 + r['directDownload'])
+                        open(r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '.zip' , 'xb').write(r2.content)
+                        os.mkdir(os.getcwd() + '\\' + r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '\\')
+                        zip = zipfile.ZipFile(r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '.zip')
+                        zip.extractall(os.getcwd() + '\\' + r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '\\')
+                    except FileExistsError:
+                        print("File already exists")
+                    except ValueError:
+                        c.privmsg(self.channel, notfound)
+
+                # The command was not recognized
             else:
-                url = 'http://ohshapes.com/api/maps/detail/'
-                try:
-                    r = requests.get(url + arg1).json()
-                    if len(str(r['stats']['rating'])[2:4]) == 1:
-                        rating = str(r['stats']['rating'])[2:4] + "0"
-                    else:
-                        rating = str(r['stats']['rating'])[2:4]
-                    c.privmsg(self.channel, r['metadata']['songName'] + ' ' + str(rating) + '% (' + r['key'] + ') was added to requests.')
-                    url2 = 'http://ohshapes.com'
-                    r2 = requests.get(url2 + r['directDownload'])
-                    open(r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '.zip' , 'xb').write(r2.content)
-                    os.mkdir(os.getcwd() + '\\' + r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '\\')
-                    zip = zipfile.ZipFile(r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '.zip')
-                    zip.extractall(os.getcwd() + '\\' + r['key'] + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['levelAuthorName']) + ' - ' + re.sub('[^A-z0-9 ]+', '', r['metadata']['songName']) + '\\')
-                except FileExistsError:
-                    print("File already exists")
-                except ValueError:  # includes simplejson.decoder.JSONDecodeError: 
-                    c.privmsg(self.channel, notfound)
-
-			# The command was not recognized
-        else:
-            print("Did not understand command: " + cmd)
+                print("Did not understand command: " + cmd)
+        except:
+            c.privmsg(self.channel, 'Something went wrong, please contact joerkig#1337 on Discord')
 
 def main():
     if len(sys.argv) != 5:
